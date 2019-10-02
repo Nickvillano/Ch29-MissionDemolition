8494ef190e9b6d00d5507f7317a2d03988f6881c
Nickvillano <32552845+Nickvillano@users.noreply.github.com>
2019-10-02 00:17:16 -0400

    Add MissionDemolition script for level management and camera modes, save scene and finish game out

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -97,6 +97,8 @@ public class Slingshot : MonoBehaviour {
             projectileRigidBody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
             projectile = null;
+            MissionDemolition.ShotFired();
+            ProjectileLine.S.poi = projectile;
         }
     }
 }
